82299e934bcf4af22a01bcd494de262860a3eacc
kenta-kenta <147038836+kenta-kenta@users.noreply.github.com>
2024-03-16 11:03:59 +0900

    Add files via upload

---
+++ seeds/index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(() => {
+        console.log('コネクションOK!');
+    })
+    .catch((err) => {
+        console.log('コネクションエラー');
+        console.log(err);
+    });
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++) {
+        const randomCityIndex = Math.floor(Math.random() * cities.length);
+        const camp = new Campground({
+            location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
+            title: `${sample(descriptors)}.${sample(places)}`
+        });
+        await camp.save();
+    }
+};
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
